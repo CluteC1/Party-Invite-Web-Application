9c65f836fbd4a2b30f755ff2cf2134b1a20055fd
Christopher <christopher.m.clute@outlook.com>
2018-10-31 13:45:15 -0400

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // this method will display a greeting to the user depending on their time
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string timeOfDay;
+            if (hour < 12)
+                timeOfDay = "Good Morning";
+            else if (hour < 18)
+                timeOfDay = "Good Afternoon";
+            else
+                timeOfDay = "Good Evening";
+            ViewBag.Greeting = timeOfDay;
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+              
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+               
+                return View();
+            }
+        }
+    }
+}
